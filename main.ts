7c780d60ab799337d798d3a180977a8d992075ef
Luther771 <110801875+Luther771@users.noreply.github.com>
2022-08-22 19:10:08 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,46 @@
+basic.showLeds(`
+    # # # # #
+    # . . . .
+    # # # # .
+    . . . . #
+    # # # # .
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . # # .
+    . # . # .
+    # . . # .
+    # # # # #
+    . . . # .
+    `)
+basic.pause(500)
+basic.showLeds(`
+    # # # # .
+    . . . # .
+    . . # . .
+    # . . # .
+    . # # . .
+    `)
+basic.pause(500)
+basic.showLeds(`
+    # # # . .
+    . . . # .
+    . # # . .
+    # . . . .
+    # # # # .
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . # . .
+    . # # . .
+    . . # . .
+    . . # . .
+    . # # # .
+    `)
+basic.showString("Good")
+basic.pause(200)
+basic.showString("Day!")
+basic.pause(200)
+basic.showString("I'M Proud")
+basic.pause(200)
+basic.showString("To Be Caritas Bosconian")
